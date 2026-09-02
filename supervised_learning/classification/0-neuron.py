daf6578eb8874a484de3fa5296ca1f30ecde5332
selenai9 <s.isimbi2@alustudent.com>
2026-09-02 16:22:37 +0000

    correct class neuron

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+"""Class Neuron that defines a single neuron performing binary classification
+"""
+
 
 import numpy as np
 
 
 class Neuron:
+    """Class Neuron"""
 
     def __init__(self, nx):
         """Instantiation function of the neuron
